49fb41c13ee2e6da9b9a416dd63ddd1abcc41074
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-26 19:43:14 -0600

    remove console log

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get("/", function (req, res) {
 });
 
 const calculateResults = function (req, res, next) {
-
   const cumulative_values = {
     depression: 0, 
     mania: 0, 
@@ -40,38 +39,19 @@ const calculateResults = function (req, res, next) {
     if (cumulative_values[associatedDomain] >=2 && !acc.includes(level_two_assesments[associatedDomain])) {
       acc.push(level_two_assesments[associatedDomain]);
     }
-    console.log(cumulative_values, "totals");
     return acc;
   }, []);
   
   console.log(results, 'results')
   res.locals.results = results
-  console.log(res.locals.results)
   next();
 };
 
 app.use(calculateResults);
 
 app.post("/post", (req, res) => {
-  // console.log(domains);
-  // Prepare output in JSON format
-  console.log(res.locals.results, 'resssssults')
   response = req.body;
-  // console.log(response);
   res.end(JSON.stringify({"results": res.locals.results}));
-  // let data = req.body;
-  // res.send("Data Received: " + JSON.stringify(data.title));
-  // //create a json response
-  // requestAsJson = JSON.stringify(req.body);
-
-  // //set the appropriate HTTP header
-  // res.setHeader("Content-Type", "application/json");
-
-  // //log the output
-  // console.log("The POST data received was: " + requestAsJson);
-
-  // //send the POST data back as JSON
-  // res.end(requestAsJson);
 });
 
 app.listen(2222);
